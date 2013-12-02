42974023fe69097f0c5ee451177138aea9a5ce7d
Marcus Brubaker <mbrubake@cs.toronto.edu>
2013-12-02 18:49:33 -0500

    Update.

--- deepnet/examples/roads_convnet/gen_data.py
+++ deepnet/examples/roads_convnet/gen_data.py
@@ -8,55 +8,117 @@ from scipy import misc
 from glob import glob
 from optparse import OptionParser
 import matplotlib.pyplot as plt
+from itertools import product
 
 if __name__ == '__main__':
+#if 0:
     cmdLineParser = OptionParser()
-    cmdLineParser.add_option('--patch_size',type='int',dest='patch_size',default=64)
-    cmdLineParser.add_option('--patch_stride',type='int',dest='patch_stride',default=8)
-    cmdLineParser.add_option('--im_start',type='int',dest='im_start',default=0)
+    cmdLineParser.add_option('--sat_patch_size',type='int',dest='sat_patch_size',default=64)
+    cmdLineParser.add_option('--map_patch_size',type='int',dest='map_patch_size',default=16)
+    cmdLineParser.add_option('--patch_stride',type='int',dest='patch_stride',default=16)
+    cmdLineParser.add_option('--num_batches',type='int',dest='num_batches',default=1)
+    cmdLineParser.add_option('--patches_per_file',type='int',dest='patches_per_file',default=2**16)
+    cmdLineParser.add_option('--randrot',type='int',dest='randrot',default=False)
+    cmdLineParser.add_option('--randoff',type='int',dest='randoff',default=False)
 
     (options, args) = cmdLineParser.parse_args()
+
+    maxpatchsize = max((options.sat_patch_size,options.map_patch_size))
+    if options.randrot:
+        maxunrotpatchsz = int(np.ceil(np.sqrt(2.0)*maxpatchsize))
+        maxunrotpatchsz = maxunrotpatchsz + (maxunrotpatchsz%2)
+    else:
+        maxunrotpatchsz = maxpatchsize
 
     indir = args[0];
     outdir = args[1];
     sat_filelist = sorted(glob(os.path.join(indir,'sat','*.tiff')))
     map_filelist = sorted(glob(os.path.join(indir,'map','*.tif')))
-    num_road_pixels = 0
-    num_pixels = 0
-    for (file_ind,(satimFile,mapimFile)) in enumerate(zip(sat_filelist,map_filelist)):
-        assert os.path.splitext(os.path.split(satimFile)[1])[0] == os.path.splitext(os.path.split(mapimFile)[1])[0]
-        print '\nImage: {0}'.format(os.path.split(satimFile)[1])
-        satim = misc.imread(satimFile)
-        mapim = misc.imread(mapimFile)
 
-        left,right,top,bottom = 0,satim.shape[1],0,satim.shape[0]
-	while np.all(satim[:,left,:] > 253): left+=1
-        while np.all(satim[:,right-1,:] > 253): right-=1
-        while np.all(satim[top,:,:] > 253): top+=1
-        while np.all(satim[bottom-1,:,:] > 253): bottom-=1
+    nSatPatchPts = options.sat_patch_size**2
+    nMapPatchPts = options.map_patch_size**2
+    sat_patchPts = np.mgrid[-options.sat_patch_size/2:options.sat_patch_size/2,
+                            -options.sat_patch_size/2:options.sat_patch_size/2].reshape((2,nSatPatchPts))
+    map_patchPts = np.mgrid[-options.map_patch_size/2:options.map_patch_size/2,
+                            -options.map_patch_size/2:options.map_patch_size/2].reshape((2,nMapPatchPts))
+ 
+    map_patchset = np.empty((options.patches_per_file,nMapPatchPts),dtype=np.float32)
+    sat_patchset = np.empty((options.patches_per_file,3*nSatPatchPts),dtype=np.float32)
+    nextPatchSetNum = 0
+    currSetNum = 0
 
-        print 'Cropping to {0},{1} x {2}, {3}'.format(left,top,right,bottom)
-        outsatim = satim[top:bottom,left:right,:]
-        outmapim = mapim[top:bottom,left:right]
+    for batchnum in range(options.num_batches):
+        for (file_ind,(satimFile,mapimFile)) in enumerate(zip(sat_filelist,map_filelist)):
+            assert os.path.splitext(os.path.split(satimFile)[1])[0] == os.path.splitext(os.path.split(mapimFile)[1])[0]
+            print '\nImage: {0}'.format(os.path.split(satimFile)[1])
+            satim = misc.imread(satimFile).astype('float32') / 255.0
+            mapim = misc.imread(mapimFile).astype('float32') / 255.0
+            valim = np.logical_not(
+                        morph.binary_dilation(\
+                            morph.binary_erosion(np.all(satim > 253.0/255.0,axis=2),iterations=8),iterations=16))
 
-        t = time.time()
-        outvalim = np.logical_not(morph.binary_dilation(morph.binary_erosion(np.all(outsatim > 253,axis=2),iterations=8),iterations=16))
-        print 'Took {0}s to detect valid pixels'.format(time.time() - t)
+            if options.randoff:
+                startx = np.random.randint(maxunrotpatchsz/2,maxpatchsize+1)
+                starty = np.random.randint(maxunrotpatchsz/2,maxpatchsize+1)
+            else:
+                startx = maxunrotpatchsz/2
+                starty = maxunrotpatchsz/2
 
-        outsatimFile = os.path.join(outdir,'sat',os.path.splitext(os.path.split(satimFile)[1])[0] + '.png')
-        outmapimFile = os.path.join(outdir,'map',os.path.splitext(os.path.split(mapimFile)[1])[0] + '.png')
-        outvalimFile = os.path.join(outdir,'val',os.path.splitext(os.path.split(mapimFile)[1])[0] + '.png')
+            range1 = range(startx,satim.shape[0] - maxunrotpatchsz/2 + 1,options.patch_stride)
+            range2 = range(starty,satim.shape[1] - maxunrotpatchsz/2 + 1,options.patch_stride)
+            patchCenters = np.array([(x,y) for (x,y) in product(range1,range2) if valim[x,y]]).T
+            nPatches = patchCenters.shape[1]
 
-        misc.imsave(outsatimFile,outsatim)
-        misc.imsave(outmapimFile,outmapim)
-        misc.imsave(outvalimFile,255*outvalim.astype('uint8'))
+            sat_patches = np.empty((satim.shape[2],nSatPatchPts,nPatches),dtype=np.float32)
+            map_patches = np.empty((1,nMapPatchPts,nPatches),dtype=np.float32)
 
-        num_pixels += np.sum(outvalim)
-        num_road_pixels += np.sum(outmapim[outvalim])
-        print '{0} out of {1} road pixels = {2}%'.format(num_road_pixels,num_pixels, float(num_road_pixels)/float(num_pixels))
+            if options.randrot:
+                angles = (2.0*np.pi)*np.random.rand(nPatches)
+                rots = np.array([[ np.cos(angles), np.sin(angles)],
+                                 [-np.sin(angles), np.cos(angles)]],dtype=np.float32)
 
-#if __name__ == '__main__':
+                map_patchCoords = np.dot(rots[:,:,datasize:datasize+nPatches].transpose((2,0,1)), \
+                                     map_patchPts.reshape((1,2,nMapPatchPts))).reshape((nPatches,2,nMapPatchPts)) \
+                                                                           .transpose((1,2,0)) \
+                                  + patchCenters[:,0:nPatches].reshape((2,1,nPatches)) + 0.5
+                sat_patchCoords = np.dot(rots[:,:,datasize:datasize+nPatches].transpose((2,0,1)), \
+                                     sat_patchPts.reshape((1,2,nSatPatchPts))).reshape((nPatches,2,nSatPatchPts)) \
+                                                                           .transpose((1,2,0)) \
+                                  + patchCenters[:,0:nPatches].reshape((2,1,nPatches)) + 0.5
+                for cdim in range(0,satim.shape[2]):
+                    spnd.interpolation.map_coordinates(satim[:,:,cdim],sat_patchCoords,output=sat_patches[cdim,:,:],order=2)
+                spnd.interpolation.map_coordinates(mapim[:,:],map_patchCoords,output=map_patches[cdim,:,:],order=2)
+            else:
+                map_patchCoords = map_patchPts.reshape((2,nMapPatchPts,1)) + patchCenters[:,0:nPatches].reshape((2,1,nPatches))
+                sat_patchCoords = sat_patchPts.reshape((2,nSatPatchPts,1)) + patchCenters[:,0:nPatches].reshape((2,1,nPatches))
+                for cdim in range(0,satim.shape[2]):
+                    sat_patches[cdim,:,:] = satim[sat_patchCoords[0,...],sat_patchCoords[1,...],cdim]
+                map_patches[0,:,:] = mapim[map_patchCoords[0,...],map_patchCoords[1,...]]
+
+            # Mean normalize satellite patches
+            sat_patches -= np.mean(sat_patches,axis=1).reshape((satim.shape[2],1,nPatches))
+
+            # Add patches to patch set
+            nCurrPatches = min((nPatches,options.patches_per_file - nextPatchSetNum))
+            map_patchset[nextPatchSetNum:nextPatchSetNum+nCurrPatches,:] = map_patches[:,:,:nCurrPatches].reshape((-1,nCurrPatches)).T
+            sat_patchset[nextPatchSetNum:nextPatchSetNum+nCurrPatches,:] = sat_patches[:,:,:nCurrPatches].reshape((-1,nCurrPatches)).T
+            nextPatchSetNum += nCurrPatches
+
+            if nextPatchSetNum == options.patches_per_file or (batchnum == options.num_batches-1 and file_ind == len(sat_filelist)-1):
+                currSetNum += 1
+                print 'Saving set number {0}'.format(currSetNum)
+                np.save('{0}/map_patches_{1:0=10}.npy'.format(outdir,currSetNum),map_patchset[:nextPatchSetNum,:])
+                np.save('{0}/sat_patches_{1:0=10}.npy'.format(outdir,currSetNum),sat_patchset[:nextPatchSetNum,:])
+
+                nextPatchSetNum = 0
+                if nPatches > nCurrPatches:
+                    map_patchset[:(nPatches-nCurrPatches),:] = map_patches[:,:,nCurrPatches:].reshape((-1,nPatches-nCurrPatches)).T
+                    sat_patchset[:(nPatches-nCurrPatches),:] = sat_patches[:,:,nCurrPatches:].reshape((-1,nPatches-nCurrPatches)).T
+                    nextPatchSetNum = nPatches-nCurrPatches
+
+
 if 0:
+#if __name__ == '__main__':
     cmdLineParser = OptionParser()
     cmdLineParser.add_option('--patch_size',type='int',dest='patch_size',default=64)
     cmdLineParser.add_option('--patch_stride',type='int',dest='patch_stride',default=8)
